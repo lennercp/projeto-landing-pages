5e1cac2cd19ebd7905587bb69a203c3319f2c66b
Lenner Coutinho Pereira <coutinholenner@gmail.com>
2023-04-25 16:57:10 -0300

    clearing tests and fix package.json

--- src/templates/App/Home.test.jsx
+++
@@ -1,8 +0,0 @@
-import { screen } from '@testing-library/react';
-import Home from '.';
-import { renderTheme } from '../../styles/render-theme';
-import { theme } from '../../styles/theme';
-
-test('renders learn react link', () => {
-  renderTheme(<Home />);
-});
