395ba88fc3bdb510711123eac3fdd340d3e5ad42
Lenner Coutinho Pereira <coutinholenner@gmail.com>
2023-04-25 14:50:38 -0300

    completed map menu

--- src/api/map-data.test.js
+++ src/api/map-data.test.js
@@ -6,8 +6,6 @@ describe('map-data', () => {
     expect(pagesData.footerHtml).toBe('');
     expect(pagesData.title).toBe('');
     expect(pagesData.slug).toBe('');
-    expect(pagesData.sections).toEqual([]);
-    expect(pagesData.menu).toEqual({});
   });
   it('should map data if there data', () => {
     const pagesData = mapData([
@@ -15,14 +13,10 @@ describe('map-data', () => {
         footer_text: '<p>hey</p>',
         title: 'title',
         slug: 'slug',
-        sections: [1, 2, 3, 4],
-        menu: { a: 'b' },
       },
     ])[0];
     expect(pagesData.footerHtml).toBe('<p>hey</p>');
     expect(pagesData.title).toBe('title');
     expect(pagesData.slug).toBe('slug');
-    expect(pagesData.sections).toEqual([1, 2, 3, 4]);
-    expect(pagesData.menu).toEqual({ a: 'b' });
   });
 });
